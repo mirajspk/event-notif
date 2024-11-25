a92c059a3a9b6ffe1a85d073602747fd97256b5a
Saksham <saksham44humagain@gmail.com>
2024-11-25 20:40:30 +0545

    Added the url for contact and about

--- backend/components/urls.py
+++ backend/components/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # Home Page
     path('', views.home , name = 'home'),
     path('events/', views.events, name= 'events'),
-    path('workshops/', views.workshops, name= 'workshops'),
+    path('about/', views.about, name= 'about'),
     path('clubs/',views.clubs, name= 'clubs'),
-    path('news/',views.news, name = 'news')
+    path('contact',views.contact, name = 'contact')
 ]
