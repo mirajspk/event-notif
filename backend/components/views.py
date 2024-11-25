3d7aea1a7c327a1729757434eabad82077e6ddcc
Saksham <saksham44humagain@gmail.com>
2024-11-25 20:40:30 +0545

    Added the url for contact and about

--- backend/components/views.py
+++ backend/components/views.py
@@ -5,12 +5,12 @@ def home(request):
 def events(request):
     return render(request, 'components/events.html')
 
-def workshops(request):
-    return render(request, 'components/workshops.html')
+def contact(request):
+    return render(request, 'components/contact.html')
 
 def clubs(request):
     return render(request,'components/clubs.html')
 
-def news(request):
-    return render(request,'components/news.html')
+def about(request):
+    return render(request,'components/about.html')
 
