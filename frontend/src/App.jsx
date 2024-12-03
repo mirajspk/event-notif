795f2c7ca024731d6ce5278c4b4c1f6810a93b2e
Miraj <147530194+mirajspk@users.noreply.github.com>
2024-12-03 23:13:51 +0545

    Experiment with buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
 import { Button } from "@/components/ui/button"
-
+import { Icons } from "./components/icons"
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
+      <Button>SIGN IN</Button>
+      <Button variant = "outline">SEE DETAILS</Button>
+      <Button variant = "ghost">SIGN IN</Button>
+      <Button variant="outline" className="bg-white text-black border-black rounded-md px-14">
+        <Icons.google />Google
+      </Button>
+      <Button variant="outline" className="bg-white text-black border-black rounded-md px-14">
+        <Icons.apple />Apple
+      </Button>
     </div>
   )
 }
