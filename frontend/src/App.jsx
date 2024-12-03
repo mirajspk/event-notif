bdf9585c93e808d2b5964a244f92617d1db0b7a6
Miraj <147530194+mirajspk@users.noreply.github.com>
2024-12-03 13:56:44 +0545

    Add shadcn/ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
+import { Button } from "@/components/ui/button"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div>
+      <Button>Click me</Button>
+    </div>
   )
 }
-
-export default App
