bfbf589b58eb0d88eefd2c1d75a55cacd77733d5
Miraj <147530194+mirajspk@users.noreply.github.com>
2024-12-04 10:38:46 +0545

    Make button prototype

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,10 @@ import { Button } from "@/components/ui/button"
 import { Icons } from "./components/icons"
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col md:flex-row gap-2 mx-10 my-10">
       <Button>SIGN IN</Button>
       <Button variant = "outline">SEE DETAILS</Button>
-      <Button variant = "ghost">SIGN IN</Button>
+      <Button variant = "ghost">LOGIN</Button>
       <Button variant="outline" className="bg-white text-black border-black rounded-md px-14">
         <Icons.google />Google
       </Button>
