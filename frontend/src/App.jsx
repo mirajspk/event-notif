1218a5c209f64665d059d5521cd33205acc593ce
Miraj <147530194+mirajspk@users.noreply.github.com>
2024-12-05 14:16:39 +0545

    Enhance button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,23 @@
 import { Button } from "@/components/ui/button"
 import { Icons } from "./components/icons"
+import { ArrowRight } from "lucide-react"
 export default function Home() {
   return (
     <div className="flex flex-col md:flex-row gap-2 mx-10 my-10">
       <Button>SIGN IN</Button>
       <Button variant = "outline">SEE DETAILS</Button>
       <Button variant = "ghost">LOGIN</Button>
-      <Button variant="outline" className="bg-white text-black border-black rounded-md px-14">
+      <Button variant="icon">
         <Icons.google />Google
       </Button>
-      <Button variant="outline" className="bg-white text-black border-black rounded-md px-14">
+      <Button variant="icon">
         <Icons.apple />Apple
       </Button>
+      <Button variant="link">
+        GET STARTED
+        <ArrowRight/>
+      </Button>
+
     </div>
   )
 }
--- frontend/src/components/ui/button.jsx
+++ frontend/src/components/ui/button.jsx
@@ -9,21 +9,19 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        default: 
+          "bg-primary text-white hover:bg-primary/80",
         outline:
-          "border border-primary text-primary bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+          "border border-primary text-primary bg-white hover:bg-primary hover:text-white",
+        ghost: "underline-offset-4 hover:underline",
+        link: "text-primary underline-offset-4 hover:underline bg-transparent text-primary",
+        icon: "bg-white text-black border border-black hover:bg-slate-200",
       },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        icon: "h-10 w-10 rounded-md",
       },
     },
     defaultVariants: {
