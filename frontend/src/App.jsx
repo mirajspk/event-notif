ffa2f8391507e08d096f0a3940710bc17dbe42e0
Miraj <147530194+mirajspk@users.noreply.github.com>
2024-12-12 11:33:36 +0545

    Make basic select

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,31 @@
-import { Button } from "@/components/ui/button"
+import * as React from "react"
 
-export default function Home() {
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+export function App() {
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+      <div className="px-5 py-5">
+        <Select>
+        <SelectTrigger className="w-[200px]">
+          <SelectValue placeholder="Select clubs" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectItem value="KUCC">Kathmandu University Computer Club (KUCC)</SelectItem>
+            <SelectItem value="KUCEC">Kathmandu University Civil Engineering Club (KUCEC)</SelectItem>
+            <SelectItem value="SEEE">Society of Electrical and Electronic Engineering (SEEE)</SelectItem>
+            <SelectItem value="KURC">Kathmandu University Robotics Club (KURC)</SelectItem>
+            <SelectItem value="KUSMC">Kathmandu University Society of Music and Culture (KUSMC)</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      </div>
   )
 }
