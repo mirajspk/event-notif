91d3195ba60e33b9b6f81be7b6362726db78d49b
Saksham <saksham44humagain@gmail.com>
2024-12-15 20:57:36 +0545

    Get request and post request

--- backend/Api/views.py
+++ backend/Api/views.py
@@ -2,7 +2,22 @@ from django.shortcuts import render
 from .serializers import EventSerializer
 from .models import Event
 from rest_framework.generics import ListAPIView # type: ignore
+from rest_framework.views import APIView # type: ignore
+# We can make a class based view with DRF's APIView
+from rest_framework.response import Response # type: ignore
+from rest_framework import status # type: ignore
+from rest_framework import viewsets # type: ignore
 
-class EventList(ListAPIView):
-    queryset = Event.objects.all()
-    serializer_class = EventSerializer
+class EventList(APIView):
+    def post(self, request):
+        serializer = EventSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+    def get(self, request):
+        events = Event.objects.all()
+        serializer = EventSerializer(events, many = True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
