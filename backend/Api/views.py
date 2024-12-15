1304d1c703612bcb04578ed68aeaed10e658afaf
Saksham <saksham44humagain@gmail.com>
2024-12-15 20:02:29 +0545

    made the view

--- backend/Api/views.py
+++ backend/Api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .serializers import EventSerializer
+from .models import Event
+from rest_framework.generics import ListAPIView # type: ignore
 
-# Create your views here.
+class EventList(ListAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
