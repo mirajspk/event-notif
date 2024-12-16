813b8e53134b64d0503fa23d49b0590dbed2668c
Miraj <147530194+mirajspk@users.noreply.github.com>
2024-12-16 10:42:35 +0545

    Create radio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
-import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 
-export default function Home() {
+
+export default function App() {
   return (
-    <div>
-      <Button>Click me</Button>
+  <RadioGroup defaultValue="option-one">
+    <div className="flex items-center space-x-2">
+      <RadioGroupItem value="option-one" id="option-one" />
+      <Label htmlFor="option-one">Next 7 days</Label>
     </div>
+    <div className="flex items-center space-x-2">
+      <RadioGroupItem value="option-two" id="option-two" />
+      <Label htmlFor="option-two">Next 30 days</Label>
+    </div>
+  </RadioGroup>
   )
 }
+
+
