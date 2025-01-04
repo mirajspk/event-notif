4b3ad0ddfe5c94dad7a886b362b9d86db0065166
ManogyaDahal <bat16692@gmail.com>
2025-01-04 20:02:20 +0545

    updated urls for added HTTP method

--- backend/Api/urls.py
+++ backend/Api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from Api import views
 urlpatterns = [
-    path('events/',views.EventList.as_view())
+    path('events/',views.EventList.as_view()),
+    path('events/<int:pk>/', views.EventList.as_view()),  
+    path('clubs/', views.ClubsView.as_view()),  
+    path('clubs/<int:pk>/', views.ClubsView.as_view()),  
 ]
