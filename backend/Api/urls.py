15e25f0a95b6bcf3ea49ba10bada9af04c57d0de
Saksham <saksham44humagain@gmail.com>
2025-01-05 17:20:29 +0545

    Added a URL for the view in urls.py

--- backend/Api/urls.py
+++ backend/Api/urls.py
@@ -3,4 +3,5 @@ from Api import views
 urlpatterns = [
     path('events/',views.EventList.as_view()),
     path('events/<int:id>/', views.EventList.as_view()), 
+    path('event-list/', views.EventListView.as_view(), name='events-list')
 ]
