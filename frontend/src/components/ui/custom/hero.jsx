30362e4aa7270c988b10e14ee7db8fcd7cacbca9
Miraj <147530194+mirajspk@users.noreply.github.com>
2025-01-19 20:49:24 +0545

    Add feature card and image

---
+++ frontend/src/components/ui/custom/hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import HeroImage from './HeroImage'
+import FeaturesSection from './FeaturesSection'
+
+const Hero = () => (
+<div className="bg-white">
+    <div className="relative w-full">
+      <div className="relative w-full">
+        <HeroImage />
+      </div>
+      <FeaturesSection />
+    </div>
+  </div>
+)
+
+export default Hero
+
