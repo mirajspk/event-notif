f821fb73db2cbcd3366711c9025a247dd73adfd8
Miraj <147530194+mirajspk@users.noreply.github.com>
2025-01-20 17:22:57 +0545

    Refactor

--- frontend/src/components/ui/custom/Header.jsx
+++ frontend/src/components/ui/custom/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/custom/customButton"
 
 const navItems = [
   { name: "Home", href: "#" },
