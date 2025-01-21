d50fb961c487f64738f21f39058d1e31b104cae6
Saksham <saksham44humagain@gmail.com>
2025-01-21 17:16:30 +0545

    Some Modification

--- backend/Api/models.py
+++ backend/Api/models.py
@@ -5,16 +5,16 @@ from django.db import models
 class Event(models.Model):
     # Choices for 'Host'
     CLUB_CHOICES = [
-        ('Coding Club', 'Coding Club'),
-        ('Music Club', 'Music Club'),
-        ('Drama Club', 'Drama Club'),
-        ('Sports Club', 'Sports Club'),
-        ('Photography Club', 'Photography Club'),
-        ('Literature Club', 'Literature Club'),
-        ('Art Club', 'Art Club'),
-        ('Environment Club', 'Environment Club'),
-        ('Robotics Club', 'Robotics Club'),
-        ('Science Club', 'Science Club'),
+        ('Association of Mechanical Engineering', 'AMES'),
+        ('Compuatiional Mathematics Club', 'KUCMC'),
+        ('Forum for Environmental Conservation and Management', 'FECAM'),
+        ('Forum of Pharmacy', 'FoP'),
+        ('Kathmandu University Architecture Students Club', 'KUARC'),
+        ('Geomatics Engineering Society', 'GES'),
+        ('Kathmandu University Circle of Noble Chemineers', 'KUCONC'),
+        ('Kathmandu University Civil Engineering Club', 'KUCEC'),
+        ('Kathmandu University Computer Club', 'KUCC'),
+        ('Natural and Social Concern Society', 'NSCS'),
     ]
 
     # Choices for 'Type'
@@ -22,12 +22,11 @@ class Event(models.Model):
         ('Event', 'Event'),
         ('Workshop', 'Workshop'),
     ]
-
     # Fields for the Event Model
-    name = models.CharField(max_length=255)  # Event/workshop name
-    location = models.CharField(max_length=255)  # Location of the event/workshop
+    name = models.CharField(max_length=2555)  # Event/workshop name
+    location = models.CharField(max_length=2555)  # Location of the event/workshop
     date = models.DateField()  # Date of the event/workshop
-    host = models.CharField(max_length=50, choices=CLUB_CHOICES)  # Host club
+    host = models.CharField(max_length=500, choices=CLUB_CHOICES)  # Host club
     type = models.CharField(max_length=50, choices=TYPE_CHOICES)  # Type: Event or Workshop
     registration_link = models.URLField(max_length=500)  # Link for registration
 
