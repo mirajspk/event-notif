cb21383a69cb510458ac09bce4f319ea756e612a
Miraj <147530194+mirajspk@users.noreply.github.com>
2025-01-22 17:09:22 +0545

    Edit footer.jsx and Refactor
    - Use TailwindCSS
    - Remove subscription form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
-import { Button } from "@/components/ui/button"
-import { Icons } from "./components/icons"
-import { ArrowRight } from "lucide-react"
-import Footer from "./components/footer/Footer"
+import Footer from "./components/Footer"
+
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen mt-[60px]">
---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Facebook, Instagram, Twitter, Linkedin } from 'lucide-react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-gray-300 p-6 md:p-8 w-full">
+      <div className="container mx-auto">
+        <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-8">
+          <div className="mb-8 md:mb-0">
+            <h2 className="text-3xl font-mono mb-6">KUevents</h2>
+            <ul className="space-y-4 md:space-y-0 md:flex md:space-x-10">
+              <li>
+                <a href="#" className="hover:text-white">Home</a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-white">Events</a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-white">Clubs</a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-white">Contact</a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-white">FAQs</a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-white">About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="flex flex-col md:flex-row justify-between items-center border-t border-gray-700 pt-6">
+          <div className="flex space-x-4 mb-4 md:mb-0">
+            <a href="#" className="hover:text-white">
+              <Facebook size={30} strokeWidth={2.25} />
+            </a>
+            <a href="#" className="hover:text-white">
+              <Instagram size={30} strokeWidth={2.25} />
+            </a>
+            <a href="#" className="hover:text-white">
+              <Twitter size={30} strokeWidth={2.25} />
+            </a>
+            <a href="#" className="hover:text-white">
+              <Linkedin size={30} strokeWidth={2.25} />
+            </a>
+          </div>
+          <p className="text-sm text-center md:text-left">
+            Copyright © KUevents 2024. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
