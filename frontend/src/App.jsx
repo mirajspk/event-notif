a4d7f405f9be292d0e88be20c220f111a0820bcb
Miraj <147530194+mirajspk@users.noreply.github.com>
2025-01-24 13:57:23 +0545

    create login form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+import LoginForm from "./components/login"
 import SignUpForm from "./components/signup"
 
 export default function Home() {
   return (
     <div>
+      <LoginForm></LoginForm>
       <SignUpForm></SignUpForm>
     </div>
   )
