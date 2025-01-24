c1807637de5e458084304f3ad9d786c1676a88dc
Girish Chaulagain <girishchaulagain5@gmail.com>
2025-01-24 13:21:49 +0545

    added User and UserRegistration serializers

--- backend/Api/serializers.py
+++ backend/Api/serializers.py
@@ -1,5 +1,25 @@
+from django.db.models.expressions import fields
 from rest_framework import serializers # type: ignore
-from .models import Event , Clubs
+from .models import Event , Clubs, User
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'user_type')
+        extra_kwargs ={'password': {'write_only':True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            user_type=validated_data['user_type']
+        )
+        return user
+
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +31,19 @@ class ClubsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Clubs
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'password', 'user_type')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            user_type=validated_data.get('user_type', 'PARTICIPANT')
+        )
+        return user
