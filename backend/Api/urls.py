24f65707ffde90366c435eccfe5e005e3fae774e
Girish Chaulagain <girishchaulagain5@gmail.com>
2025-01-24 14:16:39 +0545

    added the paths in urls.py

--- backend/Api/urls.py
+++ backend/Api/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
 from Api import views
 urlpatterns = [
-    path('events/',views.EventList.as_view())
+
+    #Events
+    path('events/',views.EventList.as_view()),
+    path('events/<int:pk>/', views.EventDetail.as_view(), name='event-detail'),
+    path('events/host/<str:host>/', views.RelatedEventsView.as_view(), name = "related_events"),
+    
+    # Event Registration
+    path('events/<int:event_id>/register/', 
+         views.EventRegistrationView.as_view(), name='event-registration'),
 ]
