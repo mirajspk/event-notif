3dc76b5c656d5d757e95cd32f28f4eaf1f14ac3d
Miraj <147530194+mirajspk@users.noreply.github.com>
2025-01-25 11:08:30 +0545

    add schedule schema

---
+++ frontend/src/lib/schedule-schema.js
@@ -0,0 +1,12 @@
+import { z } from "zod"
+
+export const eventSchema = z.object({
+  eventTitle: z.string().min(1, "Event title is required"),
+  date: z.string().min(1, "Date is required"), // Use string type for native date input
+  time: z.string().min(1, "Time is required"),
+  location: z.string().min(1, "Location is required"),
+  registrationFee: z.string().min(1, "Registration fee is required"),
+  thumbnail1: z.instanceof(File).optional(),
+  thumbnail2: z.instanceof(File).optional(),
+  description: z.string().min(1, "Description is required"),
+});
