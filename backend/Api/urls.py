aa34d55db2aae560c80c7298c72cda2e3c0ad9fb
ManogyaDahal <bat16692@gmail.com>
2025-01-31 19:03:15 +0545

    Added Changes from the previous work

--- backend/Api/urls.py
+++ backend/Api/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from Api import views
+
 urlpatterns = [
-    path('events/',views.EventList.as_view())
+    # Event endpoints
+    path('events/', views.EventList.as_view(), name='event-list'),
+    path('events/<int:id>/', views.EventList.as_view(), name='event-detail'),
+
+    # # Clubs endpoints
+    # path('clubs/', views.ClubsView.as_view(), name='club-list'),  
+    # path('clubs/<int:id>/', views.ClubsView.as_view(), name='club-detail'),  
 ]
--- backend/Api/views.py
+++ backend/Api/views.py
@@ -1,36 +1,39 @@
-from django.shortcuts import render
-from .serializers import EventSerializer , ClubsSerializer
-from .models import Event , Clubs
-from rest_framework.generics import ListAPIView # type: ignore
-from rest_framework.views import APIView # type: ignore
-# We can make a class based view with DRF's APIView
-from rest_framework.response import Response # type: ignore
-from rest_framework import status # type: ignore
-from rest_framework import viewsets # type: ignore
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import EventSerializer, ClubsSerializer
+from .models import Event, Clubs
 
 class EventList(APIView):
-    def post(self, request):
-        serializer = EventSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-
-    def get(self, request):
+    def get(self, request, id=None):
+        if id:
+            # Return single event if id is provided
+            event = get_object_or_404(Event, pk=id)
+            serializer = EventSerializer(event)
+            return Response(serializer.data)
+        
+        # Return all events if no id is provided
         events = Event.objects.all()
-        serializer = EventSerializer(events, many = True)
-        return Response(serializer.data, status = status.HTTP_200_OK)
-    
-class ClubsView(APIView):
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+
     def post(self, request):
-        serializer = ClubsSerializer(data = request.data)
+        serializer = EventSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def get(self, request):
-        events = Clubs.objects.all()
-        serializer = ClubsSerializer(events, many = True)
-        return Response(serializer.data, status = status.HTTP_200_OK)
+
+    def put(self, request, id):
+        event = get_object_or_404(Event, pk=id)
+        serializer = EventSerializer(event, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        event = get_object_or_404(Event, pk=id)
+        event.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
