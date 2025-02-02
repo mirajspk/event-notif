e5b976c7b2e24a6d1ddff1590cff04c9cc8c926d
ManogyaDahal <bat16692@gmail.com>
2025-02-02 10:24:55 +0545

    Final change from backend/GET_POST

--- backend/Events/urls.py
+++ backend/Events/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('components.urls')),
     path('Api/', include("Api.urls"))   
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
