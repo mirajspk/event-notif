5058d84e517b3c9648481d5b9509902e6cf2a17f
ManogyaDahal <bat16692@gmail.com>
2025-02-13 13:26:42 +0545

    added .env and secured credentials

--- backend/Events/settings.py
+++ backend/Events/settings.py
@@ -2,9 +2,18 @@ import os
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-
+import environ
 
 load_dotenv()
+
+load_dotenv()  
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == "True"
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 
 
@@ -186,7 +195,4 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'girishchaulagain5@gmail.com'
-EMAIL_HOST_PASSWORD = 'mcvg atro vdqm otby'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
