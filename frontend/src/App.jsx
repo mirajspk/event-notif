1fa91a3540ecfe3dbc10da86f6a5f97d7e9d3135
ManogyaDahal <bat16692@gmail.com>
2025-02-13 09:38:41 +0545

    Updated code for Upcoming events

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,15 @@
 //Importing routers and pageNotfound page
 import { Route,  Routes } from "react-router-dom"
-import EventList from "./components/ui/custom/eventcardAll"
+
+import EventcardAll from "./components/ui/custom/eventcardAll"
+import EventcardPastEvent from "./components/ui/custom/eventcardPastEvents"
+import EventcardPastWorkshop from "./components/ui/custom/eventcardPastWorkshop"
+import EventcardSevenEvent from "./components/ui/custom/eventcardSevenEvent"
+import EventcardSevenWorkshop from "./components/ui/custom/eventcardSevenWorkshop"
+import EventcardThirtyEvent from "./components/ui/custom/eventcardThirtyEvent"
+import EventcardThirtyWorkshop from "./components/ui/custom/eventcardThirtyWorkshop"
+import ThreeEvent from "./components/ui/custom/eventcardThree"
+
 import ClubPage from "./components/ui/custom/ClubPage"
 import Club from "./components/ui/custom/Club"
 import PageNotFound from"./components/pageNotFound"
@@ -19,14 +28,22 @@ const App = () => {
   return (
     <div className="flex flex-wrap justify-center my-9 gap-9">
       <Routes>
-        <Route path="/events" element={<EventList /> } />
+        <Route path="/events" element={<EventcardAll/> } />
         <Route path="/club" element={<Club/>}/>
         <Route path="/clubpage" element={<ClubPage/>}/>
         <Route path="/login" element={<LoginForm />}/>
         <Route path="/signup" element={<SignUpForm />}/>
         <Route path="*" element={<PageNotFound /> } />
-
         <Route path="/events/:id" element={<EventDetail />} />
+
+
+        <Route path="/pastworkshop" element={<EventcardPastWorkshop/> } />
+        <Route path="/pastevent" element={<EventcardPastEvent/> } />
+        <Route path="/eventseven" element={<EventcardSevenEvent/> } />
+        <Route path="/workshopseven" element={<EventcardSevenWorkshop/> } />
+        <Route path="/eventthirty" element={<EventcardThirtyEvent/> } />
+        <Route path="/workshopthirty" element={<EventcardThirtyWorkshop/> } />
+        <Route path="/three" element={<ThreeEvent/> } />
       </Routes>
     </div>
   )  
