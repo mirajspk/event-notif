14006cda5cffc4349f9f84059f411f790b1d9616
Lazy-MoMo <abhisekhjoshi74@gmail.com>
2025-02-21 16:01:55 +0545

    PROJECT FINISHED
    maybe

--- frontend/src/components/ui/custom/Club.jsx
+++ frontend/src/components/ui/custom/Club.jsx
@@ -23,7 +23,7 @@ export default function Club({ clubname, clubId, clubImage }) {
 
   const handleSubscribe = async (data) => {
     try {
-      const response = await fetch("/api/subscribe/", {
+      const response = await fetch("http://127.0.0.1:8000/api/subscribe/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
